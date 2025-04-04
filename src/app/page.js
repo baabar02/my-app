2b5399f62d3a9c2d79f6de8b190680874934bf68
baabar02 <baabar02githu.com>
2025-04-04 08:58:56 +0800

    sss

--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,63 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+//global scope
+//loacl scope
+
+const value = 1;
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  console.log(value);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+  return (
+    <div
+      className="ss"
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        width: 500,
+        marginLeft: 30,
+        marginTop: 30,
+      }}
+    >
+      <p>Hello</p>
+      <p>Hello</p>
+      <Box />
+      <Box1 />
+      <Box2 />
     </div>
   );
 }
+
+const Box = () => {
+  return (
+    <div
+      style={{
+        justifyItems: "center",
+        height: "50px",
+        width: "50px",
+        backgroundColor: "grey",
+      }}
+    >
+      box
+    </div>
+  );
+};
+
+const Box1 = () => {
+  return (
+    <div style={{ height: "50px", width: "50px", backgroundColor: "yellow" }}>
+      box
+    </div>
+  );
+};
+
+const Box2 = () => {
+  return (
+    <div style={{ height: "50px", width: "50px", backgroundColor: "green" }}>
+      box
+    </div>
+  );
+};
+
+const getValue = (value) => {
+  return value;
+};
+console.log(getValue);
